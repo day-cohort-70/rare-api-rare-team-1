7b6c2138f40848207e6b84b9b208d2b007abf60a
William Woodard <williamwoodard98@gmail.com>
2024-05-30 14:06:12 -0500

    added a get method to get all categories

--- json-server.py
+++ json-server.py
@@ -5,6 +5,7 @@ from request_handler import HandleRequests, status
 #import from views below
 
 from views import create_user, login_user
+from views import grabCategoryList
 
 
 class JSONServer(HandleRequests):
@@ -12,9 +13,15 @@ class JSONServer(HandleRequests):
 
     def do_GET(self):
         """Handle GET requests from a client"""
-        # response_body = ""
-        # url = self.parse_url(self.path)
+        response_body = ""
+        url = self.parse_url(self.path)
 
+        if url["requested_resource"] == "category":
+            if url["pk"] != 0:
+                pass
+
+            response_body = grabCategoryList()
+            return self.response(response_body, status.HTTP_200_SUCCESS.value)
         # else:
         return self.response("", status.HTTP_404_CLIENT_ERROR_RESOURCE_NOT_FOUND.value)
     
---
+++ views/category.py
@@ -0,0 +1,24 @@
+import sqlite3
+import json
+from datetime import datetime
+
+def grabCategoryList():
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.label
+        FROM Categories c
+        """)
+        query_results = db_cursor.fetchall()
+
+        categories = []
+        for row in query_results:
+            categories.append(dict(row))
+
+        serialized_categories = json.dumps(categories)
+
+    return serialized_categories
